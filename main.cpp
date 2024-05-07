4811c5349abc994fbe2d1eff7f0d1c0f455786c8
Guy Marshall <guymarshall9@gmail.com>
2024-05-07 22:11:21 +0100

    UPDATE: Remove timing as it was just to help optimise the code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <chrono>
 #include <cmath>
 #include <vector>
 #include <algorithm>
@@ -13,15 +12,8 @@
 constinit boost::multiprecision::cpp_int user_input = 276;
 
 int main() {
-    const std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-
     boost::multiprecision::cpp_int number_of_steps = get_number_of_steps(user_input);
     std::cout << "Number of steps for " << user_input << ": " << number_of_steps << std::endl;
-
-    const std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-
-    const std::chrono::duration<double> duration = end - begin;
-    std::cout << "Duration: " << duration.count() << " seconds" << std::endl;
 
     return 0;
 }
